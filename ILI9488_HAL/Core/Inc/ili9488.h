a84d236392f5dc38b717cf00eed838ddd0cf627e
misty <mistanother@gmail.com>
2023-05-15 10:20:53 +0500

    ILI9488

---
+++ ILI9488_HAL/Core/Inc/ili9488.h
@@ -0,0 +1,220 @@
+#ifndef _ILI9488_H_
+#define _ILI9488_H_
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_gpio.h"
+#include "stm32f1xx_hal_rcc.h"
+#include "stm32f1xx_hal_spi.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#define	LCD_SPI hspi2
+
+#define LCD_HEIGHT 320
+#define LCD_WIDTH  480
+
+#define DEFAULT_ORIENTATION 1
+
+#define SOFT_CS 0
+#define LCD_CS_PORT	 GPIOB
+#define LCD_CS_PIN	 GPIO_PIN_12
+#define LCD_DC_PORT	 GPIOB
+#define LCD_DC_PIN	 GPIO_PIN_1
+#define LCD_RST_PORT GPIOB
+#define LCD_RST_PIN	 GPIO_PIN_0
+#define LCD_S_CS	HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_SET);
+#define LCD_U_CS	HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_RESET);
+#define LCD_S_DC	HAL_GPIO_WritePin(LCD_DC_PORT, LCD_DC_PIN, GPIO_PIN_SET);
+#define LCD_U_DC	HAL_GPIO_WritePin(LCD_DC_PORT, LCD_DC_PIN, GPIO_PIN_RESET);
+#define LCD_S_RST	HAL_GPIO_WritePin(LCD_RST_PORT, LCD_RST_PIN, GPIO_PIN_SET);
+#define LCD_U_RST	HAL_GPIO_WritePin(LCD_RST_PORT, LCD_RST_PIN, GPIO_PIN_RESET);
+
+#define BLACK   0x000000
+#define WHITE   0xFFFFFF
+#define RED     HUE_17
+#define BLUE    HUE_01
+#define BLUE_D  0x0000A0 /*   0,   0, 160 */
+#define CYAN    HUE_05
+#define YELLOW  HUE_13 /* 255, 255,   0 */
+#define MAGENTA HUE_21 /* 255,   0, 255 */
+#define GREEN   HUE_09 /*   0, 255,   0 */
+#define GREEN_D 0x007F00 /*   0, 128,   0 */
+#define PURPLE  0x7F007F /* 128,   0, 128 */
+#define TEAL    0x007F7F /*   0, 128, 128 */
+#define NAVY    0x00007F /*   0,   0, 128 */
+#define SILVER  0xBFBFBF /* 191, 191, 191 */
+#define GRAY    0x7F7F7F /* 128, 128, 128 */
+#define ORANGE  0xFFA500 /* 255, 165,   0 */
+#define BROWN   0xA52A2A /* 165, 255,  42 */
+#define MAROON  0x7F0000 /* 128,   0,   0 */
+#define OLIVE   0x7F7F00 /* 128, 128,   0 */
+#define LIME    HUE_12
+
+#define HUE_01 0x0000FF // 000, 000, 255 - BLUE
+#define HUE_02 0x003FFF // 000, 063, 255 - 
+#define HUE_03 0x007FFF // 000, 127, 255 - 
+#define HUE_04 0x00BFFF // 000, 191, 255 - 
+#define HUE_05 0x00FFFF // 000, 255, 255 - CYAN
+#define HUE_06 0x00FFBF // 000, 255, 191 - 
+#define HUE_07 0x00FF7F // 000, 255, 127 - 
+#define HUE_08 0x00FF00 // 000, 255, 063 - 
+#define HUE_09 0x00FF00 // 000, 255, 000 - GREEN
+#define HUE_10 0x3FFF00 // 063, 255, 000 - 
+#define HUE_11 0x7FFF7F // 127, 255, 000 - 
+#define HUE_12 0xBFFF00 // 191, 255, 000 - LIME
+#define HUE_13 0xFFFF00 // 255, 255, 000 - YELLOW
+#define HUE_14 0xFFBF00 // 255, 191, 000 - 
+#define HUE_15 0xFF7F00 // 255, 127, 000 - 
+#define HUE_16 0xFF3F00 // 255, 063, 000 - 
+#define HUE_17 0xFF0000 // 255, 000, 000 - RED
+#define HUE_18 0xFF003F // 255, 000, 063 - 
+#define HUE_19 0xFF007F // 255, 000, 127 - 
+#define HUE_20 0xFF00BF // 255, 000, 191 - 
+#define HUE_21 0xFF00FF // 255, 000, 255 - MAGENTA
+#define HUE_22 0xBF00FF // 191, 000, 255 - 
+#define HUE_23 0x7F00FF // 127, 000, 255 - 
+#define HUE_24 0x3F00FF // 063, 000, 255 - 
+
+typedef struct { // Data stored PER GLYPH
+	uint16_t bitmapOffset;     // Pointer into GFXfont->bitmap
+	uint8_t  width, height;    // Bitmap dimensions in pixels
+	uint8_t  xAdvance;         // Distance to advance cursor (x axis)
+	int8_t   xOffset, yOffset; // Dist from cursor position to UL corner
+} GFXglyph;
+
+typedef struct { // Data stored for FONT AS A WHOLE:
+	uint8_t  *bitmap;      // Glyph bitmaps, concatenated
+	GFXglyph *glyph;       // Glyph array
+	uint8_t   first, last; // ASCII extents
+	uint8_t   yAdvance;    // Newline distance (y axis)
+} GFXfont;
+
+uint32_t RGB(uint8_t r, uint8_t g, uint8_t b);
+
+void LCD_Init(void);
+void LCD_Orientation(uint8_t orientation);
+void LCD_Pixel(uint16_t x, uint16_t y, uint32_t color24);
+void LCD_Rect_Fill(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint32_t color24);
+void LCD_Line(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t size, uint32_t color24);
+void LCD_Rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t size, uint32_t color24);
+void LCD_Triangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3, uint8_t size, uint32_t color24);
+void LCD_Triangle_Fill(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t x3, uint16_t y3, uint32_t color24);
+void LCD_Ellipse(int16_t x0, int16_t y0, int16_t rx, int16_t ry, uint8_t fill, uint8_t size, uint32_t color24);
+void LCD_Circle(uint16_t x, uint16_t y, uint8_t radius, uint8_t fill, uint8_t size, uint32_t color24);
+void LCD_Rect_Round(uint16_t x, uint16_t y, uint16_t length, uint16_t width, uint16_t r, uint8_t size, uint32_t color24);
+void LCD_Rect_Round_Fill(uint16_t x, uint16_t y, uint16_t length, uint16_t width, uint16_t r, uint32_t color24);
+void LCD_Font(uint16_t x, uint16_t y, const char *text, const GFXfont *p_font, uint8_t size, uint32_t color24);
+void LCD_Bitmap(uint16_t x, uint16_t y, const char *bitmap);
+void LCD_Bitmap_Mono(uint16_t x, uint16_t y, const char *bitmap, uint32_t color24_set, uint32_t color24_unset);
+
+void LCD_setupScrollArea(uint16_t TFA, uint16_t BFA) ;
+void LCD_scrollAddress(uint16_t VSP);
+int LCD_scrollLine(void);
+
+// The scrolling area must be a integral multiple of TEXT_HEIGHT
+#define TEXT_HEIGHT 16 // Height of text to be printed and scrolled
+#define BOT_FIXED_AREA 0 // Number of lines in bottom fixed area (lines counted from bottom of screen)
+#define TOP_FIXED_AREA 16 // Number of lines in top fixed area (lines counted from top of screen)
+
+// The scrolling area must be a integral multiple of TEXT_HEIGHT
+#define TEXT_HEIGHT 16 // Height of text to be printed and scrolled
+#define BOT_FIXED_AREA 0 // Number of lines in bottom fixed area (lines counted from bottom of screen)
+#define TOP_FIXED_AREA 16 // Number of lines in top fixed area (lines counted from top of screen)
+
+/* Level 1 Commands (from the display Datasheet) */
+#define NOP                             0x00
+#define SOFTWARE_RESET                  0x01
+#define READ_DISP_ID                    0x04
+#define READ_DISP_STATUS                0x09
+#define READ_DISP_MADCTRL               0x0B
+#define READ_DISP_PIXEL_FORMAT          0x0C
+#define READ_DISP_IMAGE_FORMAT          0x0D
+#define READ_DISP_SIGNAL_MODE           0x0E
+#define READ_DISP_SELF_DIAGNOSTIC       0x0F
+#define ENTER_SLEEP_MODE                0x10
+#define SLEEP_OUT                       0x11
+#define PARTIAL_MODE_ON                 0x12
+#define NORMAL_DISP_MODE_ON             0x13
+#define DISP_INVERSION_OFF              0x20
+#define DISP_INVERSION_ON               0x21
+#define GAMMA_SET                       0x26
+#define DISPLAY_OFF                     0x28
+#define DISPLAY_ON                      0x29
+#define COLUMN_ADDRESS_SET              0x2A
+#define PAGE_ADDRESS_SET                0x2B
+#define MEMORY_WRITE                    0x2C
+#define COLOR_SET                       0x2D
+#define MEMORY_READ                     0x2E
+#define PARTIAL_AREA                    0x30
+#define VERT_SCROLL_DEFINITION          0x33
+#define TEARING_EFFECT_LINE_OFF         0x34
+#define TEARING_EFFECT_LINE_ON          0x35
+#define MEMORY_ACCESS_CONTROL           0x36
+#define VERT_SCROLL_START_ADDRESS       0x37
+#define IDLE_MODE_OFF                   0x38
+#define IDLE_MODE_ON                    0x39
+#define COLMOD_PIXEL_FORMAT_SET         0x3A
+#define WRITE_MEMORY_CONTINUE           0x3C
+#define READ_MEMORY_CONTINUE            0x3E
+#define SET_TEAR_SCANLINE               0x44
+#define GET_SCANLINE                    0x45
+#define WRITE_DISPLAY_BRIGHTNESS        0x51
+#define READ_DISPLAY_BRIGHTNESS         0x52
+#define WRITE_CTRL_DISPLAY              0x53
+#define READ_CTRL_DISPLAY               0x54
+#define WRITE_CONTENT_ADAPT_BRIGHTNESS  0x55
+#define READ_CONTENT_ADAPT_BRIGHTNESS   0x56
+#define WRITE_MIN_CAB_LEVEL             0x5E
+#define READ_MIN_CAB_LEVEL              0x5F
+#define READ_ID1                        0xDA
+#define READ_ID2                        0xDB
+#define READ_ID3                        0xDC
+/* Level 2 Commands (from the display Datasheet) */
+#define RGB_SIGNAL_CONTROL              0xB0
+#define FRAME_RATE_CONTROL_NORMAL       0xB1
+#define FRAME_RATE_CONTROL_IDLE_8COLOR  0xB2
+#define FRAME_RATE_CONTROL_PARTIAL      0xB3
+#define DISPLAY_INVERSION_CONTROL       0xB4
+#define BLANKING_PORCH_CONTROL          0xB5
+#define DISPLAY_FUNCTION_CONTROL        0xB6
+#define ENTRY_MODE_SET                  0xB7
+#define BACKLIGHT_CONTROL_1             0xB8
+#define BACKLIGHT_CONTROL_2             0xB9
+#define BACKLIGHT_CONTROL_3             0xBA
+#define BACKLIGHT_CONTROL_4             0xBB
+#define BACKLIGHT_CONTROL_5             0xBC
+#define BACKLIGHT_CONTROL_6             0xBD
+#define BACKLIGHT_CONTROL_7             0xBE
+#define BACKLIGHT_CONTROL_8             0xBF
+#define POWER_CONTROL_1                 0xC0
+#define POWER_CONTROL_2                 0xC1
+#define VCOM_CONTROL_1                  0xC5
+#define VCOM_CONTROL_2                  0xC7
+#define POWER_ON_SEQ_CONTROL            0xCB
+#define POWER_CONTROL_A                 0xCD
+#define POWER_CONTROL_B                 0xCF
+#define NVMEM_WRITE                     0xD0
+#define NVMEM_PROTECTION_KEY            0xD1
+#define NVMEM_STATUS_READ               0xD2
+#define READ_ID4                        0xD3
+#define POSITIVE_GAMMA_CORRECTION       0xE0
+#define NEGATIVE_GAMMA_CORRECTION       0xE1
+#define DIGITAL_GAMMA_CONTROL_1         0xE2
+#define DIGITAL_GAMMA_CONTROL_2         0xE3
+#define DRIVER_TIMING_CONTROL_A         0xE8
+#define DRIVER_TIMING_CONTROL_B         0xEA
+#define POWER_ON_SEQUENCE				0xED
+#define ENABLE_3_GAMMA_CONTROL          0xF2
+#define INTERFACE_CONTROL               0xF6
+#define PUMP_RATIO_CONTROL              0xF7
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _ILI9488_H_ */
---
+++ ILI9488_HAL/Core/Src/main.c
@@ -0,0 +1,479 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "ili9488.h"
+#include "xpt2046.h"
+
+#include "fonts/Font_3_Tiny.h"
+#include "fonts/Font_3_PicoPixel.h"
+#include "fonts/Font_3_TomThumb_Extended.h"
+#include "fonts/Font_5_Org.h"
+#include "fonts/Font_8_Sinclair.h"
+#include "fonts/Font_8_Sinclair_Inverted.h"
+#include "fonts/Font_8_Tiny.h"
+#include "fonts/Font_8_Myke.h"
+#include "fonts/Font_8_Default.h"
+#include "fonts/Font_8_Retro.h"
+#include "fonts/Font_9_Mono.h"
+#include "fonts/Font_9_Mono_Bold.h"
+#include "fonts/Font_9_Mono_Bold_Oblique.h"
+#include "fonts/Font_9_Mono_Oblique.h"
+#include "fonts/Font_9_Sans.h"
+#include "fonts/Font_9_Sans_Bold.h"
+#include "fonts/Font_9_Sans_Bold_Oblique.h"
+#include "fonts/Font_9_Sans_Oblique.h"
+#include "fonts/Font_9_Serif.h"
+#include "fonts/Font_9_Serif_Bold.h"
+#include "fonts/Font_9_Serif_Bold_Italic.h"
+#include "fonts/Font_9_Serif_Italic.h"
+#include "fonts/Font_12_Mono.h"
+#include "fonts/Font_12_Mono_Bold.h"
+#include "fonts/Font_12_Mono_Bold_Oblique.h"
+#include "fonts/Font_12_Mono_Oblique.h"
+#include "fonts/Font_12_Sans.h"
+#include "fonts/Font_12_Sans_Bold.h"
+#include "fonts/Font_12_Sans_Bold_Oblique.h"
+#include "fonts/Font_12_Sans_Oblique.h"
+#include "fonts/Font_12_Serif.h"
+#include "fonts/Font_12_Serif_Bold.h"
+#include "fonts/Font_12_Serif_Bold_Italic.h"
+#include "fonts/Font_12_Serif_Italic.h"
+#include "fonts/Font_16_Arial.h"
+#include "fonts/Font_16_Arial_Bold.h"
+#include "fonts/Font_16_Arial_Italic.h"
+#include "fonts/Font_16_Default.h"
+#include "fonts/Font_16_FranklinGothic.h"
+#include "fonts/Font_16_Hallfetica.h"
+#include "fonts/Font_16_Nadianne.h"
+#include "fonts/Font_16_Sinclair.h"
+#include "fonts/Font_16_Sinclair_Inverted.h"
+#include "fonts/Font_16_Swiss_Outline.h"
+#include "fonts/Font_16_Matrix_Full.h"
+#include "fonts/Font_16_Matrix_Full_Slash.h"
+#include "fonts/Font_16_Matrix_Num.h"
+#include "fonts/Font_16_Arial_Round.h"
+#include "fonts/Font_16_OCR.h"
+#include "fonts/Font_16_Segment_16_Full.h"
+#include "fonts/Font_16_Grotesk.h"
+#include "fonts/Font_16_Grotesk_Bold.h"
+#include "fonts/Font_16_Retro.h"
+#include "fonts/Font_18_Mono.h"
+#include "fonts/Font_18_Mono_Bold.h"
+#include "fonts/Font_18_Mono_Bold_Oblique.h"
+#include "fonts/Font_18_Mono_Oblique.h"
+#include "fonts/Font_18_Sans.h"
+#include "fonts/Font_18_Sans_Bold.h"
+#include "fonts/Font_18_Sans_Bold_Oblique.h"
+#include "fonts/Font_18_Sans_Oblique.h"
+#include "fonts/Font_18_Serif.h"
+#include "fonts/Font_18_Serif_Bold.h"
+#include "fonts/Font_18_Serif_Bold_Italic.h"
+#include "fonts/Font_18_Serif_Italic.h"
+#include "fonts/Font_24_Matrix_Num.h"
+#include "fonts/Font_24_Inconsola.h"
+#include "fonts/Font_24_Ubuntu.h"
+#include "fonts/Font_24_Ubuntu_Bold.h"
+#include "fonts/Font_24_Segment_16_Full.h"
+#include "fonts/Font_24_Mono.h"
+#include "fonts/Font_24_Mono_Bold.h"
+#include "fonts/Font_24_Mono_Bold_Oblique.h"
+#include "fonts/Font_24_Mono_Oblique.h"
+#include "fonts/Font_24_Grotesk.h"
+#include "fonts/Font_24_Grotesk_Bold.h"
+#include "fonts/Font_24_Sans.h"
+#include "fonts/Font_24_Sans_Bold.h"
+#include "fonts/Font_24_Sans_Bold_Oblique.h"
+#include "fonts/Font_24_Sans_Oblique.h"
+#include "fonts/Font_24_Serif.h"
+#include "fonts/Font_24_Serif_Bold.h"
+#include "fonts/Font_24_Serif_Bold_Italic.h"
+#include "fonts/Font_24_SerifItalic.h"
+#include "fonts/Font_32_Dingbats.h"
+#include "fonts/Font_32_Special.h"
+#include "fonts/Font_32_Calibri_Bold.h"
+#include "fonts/Font_32_Arial_Num_Plus.h"
+#include "fonts/Font_32_Matrix_Num.h"
+#include "fonts/Font_32_Segment_7_Full.h"
+#include "fonts/Font_32_Segment_7_Num_Plus.h"
+#include "fonts/Font_32_Segment_16_Full.h"
+#include "fonts/Font_32_Segment_18_Full.h"
+#include "fonts/Font_32_Grotesk.h"
+#include "fonts/Font_32_Grotesk_Bold.h"
+#include "fonts/Font_40_Segment_16_Full.h"
+#include "fonts/Font_48_Battery.h"
+#include "fonts/Font_48_Segment_16_Num.h"
+#include "fonts/Font_64_Segment_16_Num.h"
+#include "fonts/Font_64_Segment_7_Num.h"
+//#include "fonts/Font_96_Segment_7_Num.h"
+//#include "fonts/Font_96_Segment_16_Num.h"
+//#include "fonts/Font_128_Segment_16_Num.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi1;
+SPI_HandleTypeDef hspi2;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+uint8_t touchIRQ = 0;
+uint16_t touchX = 0, touchY = 0;
+uint64_t millis = 0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI1_Init(void);
+static void MX_SPI2_Init(void);
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SPI1_Init();
+  MX_SPI2_Init();
+  /* USER CODE BEGIN 2 */
+
+	LCD_Init();
+	XPT2046_Init();
+
+	LCD_Rect_Fill(0, 0, LCD_WIDTH, LCD_HEIGHT, BLUE);
+	LCD_Rect_Fill(1, 1, LCD_WIDTH -2, LCD_HEIGHT -2, BLACK);
+
+//	LCD_Rect_Fill(0, 0, 320, 240, BLUE);
+//	LCD_Rect_Fill(1, 1, 318, 238, BLACK);
+
+	HAL_Delay(250);
+	LCD_Rect_Fill(0, 0, 160, 128, BLACK);
+	for (uint8_t x = 8; x <= 160; x += 8) {
+		LCD_Line(0, 0, x, 128, 1, GREEN);
+	}
+	for (uint8_t y = 8; y <= 128; y += 8) {
+		LCD_Line(0, 0, 160, y, 1, GREEN);
+	}
+	HAL_Delay(250);
+
+	uint8_t h = 16;
+	uint8_t w = 20;
+	for (uint8_t i = 0; i < 8; i++) {
+		LCD_Rect(80 - w / 2, 64 - h / 2, w, h, 2, YELLOW);
+		h += 16;
+		w += 20;
+	}
+	HAL_Delay(250);
+	LCD_Rect_Fill(0, 0, 160, 128, BLUE);
+	LCD_Rect_Fill(1, 1, 158, 126, BLACK);
+	LCD_Font(5, 40, "This is\n just a Test\n TomThumb Ext\n",
+			_3_TomThumb_Extended, 1, YELLOW);
+	LCD_Line(23, 20, 137, 20, 1, MAGENTA);
+	LCD_Line(23, 21, 137, 21, 1, BLUE);
+	LCD_Line(23, 21, 137, 21, 1, BLUE);
+	LCD_Font(41, 10, "DISPLAY DRIVER", _5_Org, 1, MAGENTA);
+	LCD_Font(45, 35, "SERIF BOLD", _9_Serif_Bold, 1, RED);
+	LCD_Circle(40, 90, 30, 0, 1, RED);
+	LCD_Circle(45, 90, 20, 1, 1, BLUE);
+	LCD_Triangle_Fill(5, 5, 5, 20, 25, 25, BLUE);
+	LCD_Triangle(5, 5, 5, 20, 25, 25, 1, RED);
+	LCD_Rect(60, 45, 30, 20, 2, GREEN);
+	LCD_Rect_Round(80, 70, 60, 25, 10, 3, WHITE);
+	LCD_Rect_Round_Fill(80, 100, 60, 25, 10, WHITE);
+	LCD_Ellipse(60, 100, 30, 20, 0, 2, YELLOW);
+	LCD_Ellipse(125, 60, 25, 15, 1, 1, YELLOW);
+	LCD_Font(0, 200, "1234567890", _32_Segment_7_Num_Plus, 1, RED);
+	LCD_Font(10, 220, "1234567890 Default Font", _8_Default, 1, RED);
+
+	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while (1) {
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+		if ((HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_3) == GPIO_PIN_SET))
+			touchIRQ = 1;
+
+		touchIRQ = 1;
+
+		if (touchIRQ) {
+			touchX = getX();
+			touchY = getY();
+			if (touchX && touchY && touchY != 0x0DB) {
+				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+				LCD_Rect_Fill(touchX, touchY, 1, 1, WHITE);
+			}
+			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+			touchX = 0;
+			touchY = 0;
+			touchIRQ = 0;
+		}
+	}
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief SPI1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI1_Init(void)
+{
+
+  /* USER CODE BEGIN SPI1_Init 0 */
+
+  /* USER CODE END SPI1_Init 0 */
+
+  /* USER CODE BEGIN SPI1_Init 1 */
+
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_HARD_OUTPUT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI1_Init 2 */
+
+  /* USER CODE END SPI1_Init 2 */
+
+}
+
+/**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_HARD_OUTPUT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LCD_DC_GPIO_Port, LCD_DC_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PC13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : TOUCH_IRQ_Pin */
+  GPIO_InitStruct.Pin = TOUCH_IRQ_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(TOUCH_IRQ_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LCD_DC_Pin */
+  GPIO_InitStruct.Pin = LCD_DC_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LCD_DC_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	while (1) {
+	}
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
