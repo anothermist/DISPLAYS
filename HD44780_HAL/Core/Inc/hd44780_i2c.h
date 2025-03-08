1b71c089e544fc185f52ae92e85d2100078d7f94
anothermist <mistanother@gmail.com>
2025-03-09 02:12:49 +0500

    Code Formatting

--- HD44780_HAL/Core/Inc/hd44780_i2c.h
+++ HD44780_HAL/Core/Inc/hd44780_i2c.h
@@ -1,14 +1,16 @@
+#ifndef HD44780_I2C_H_
+#define HD44780_I2C_H_
+
 #include "stm32f1xx_hal.h"
 
-void lcd_init (void);   // initialize lcd
+#define HD44780_INTERFACE hi2c1
+#define HD44780_ADDRESS 0x4E
 
-void lcd_send_cmd (char cmd);  // send command to the lcd
+void HD44780_Init(void);   // initialize lcd
+void HD44780_Command(char cmd);  // send command to the lcd
+void HD44780_Data(char data);  // send data to the lcd
+void HD44780_String(char *str);  // send string to the lcd
+void HD44780_Position(int row, int col); // put cursor at the entered position row (0 or 1), col (0-15);
+void HD44780_Clear(void);
 
-void lcd_send_data (char data);  // send data to the lcd
-
-void lcd_send_string (char *str);  // send string to the lcd
-
-void lcd_put_cur(int row, int col);  // put cursor at the entered position row (0 or 1), col (0-15);
-
-void lcd_clear (void);
-
+#endif /* HD44780_I2C_H_ */
--- HD44780_HAL/Core/Src/main.c
+++ HD44780_HAL/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2022 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -59,185 +59,165 @@ static void MX_I2C1_Init(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_I2C1_Init();
-  /* USER CODE BEGIN 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_I2C1_Init();
+	/* USER CODE BEGIN 2 */
 
-  lcd_init ();
-  lcd_put_cur(0, 0);
-  lcd_send_string("*** STRING 0 ***");
+	HD44780_Init();
+	HD44780_Clear();
 
-  char cc[] = {
-  		0x00, 0x00, 0x0A, 0x00, 0x11, 0x0E, 0x00, 0x00,  // smiley
-  		0x0E, 0x0E, 0x04, 0x0E, 0x15, 0x04, 0x0A, 0x0A,  // Robo
-  		0x08, 0x0C, 0x0E, 0x0F, 0x0E, 0x0C, 0x08, 0x00,  // arrow
-  		0x00, 0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x04, 0x00,  // bell
-  		0x00, 0x00, 0x0A, 0x15, 0x11, 0x0E, 0x04, 0x00,  // Heart
-  		0x00, 0x0E, 0x11, 0x11, 0x11, 0x0A, 0x1B, 0x00,  // omega
-  		0x0E, 0x10, 0x17, 0x12, 0x12, 0x12, 0x10, 0x0E,  // CT
-  		0x04, 0x04, 0x1F, 0x04, 0x04, 0x00, 0x1F, 0x00  // +-
-  };
-  lcd_send_cmd(0x40);
-  for (int i=0; i<64; i++) lcd_send_data(cc[i]);
+	HD44780_SetPos(0, 0); HD44780_String("*** STRING 0 ***");
+	HD44780_SetPos(1, 0); HD44780_String("*** STRING 1 ***");
 
+	HAL_Delay(1000);
+	HD44780_Clear();
 
-  lcd_put_cur(1, 1);
-  lcd_send_data(0);
+	char cc[] = { 0x00, 0x00, 0x0A, 0x00, 0x11, 0x0E, 0x00, 0x00,  // smiley
+			0x0E, 0x0E, 0x04, 0x0E, 0x15, 0x04, 0x0A, 0x0A,  // Robo
+			0x08, 0x0C, 0x0E, 0x0F, 0x0E, 0x0C, 0x08, 0x00,  // arrow
+			0x00, 0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x04, 0x00,  // bell
+			0x00, 0x00, 0x0A, 0x15, 0x11, 0x0E, 0x04, 0x00,  // Heart
+			0x00, 0x0E, 0x11, 0x11, 0x11, 0x0A, 0x1B, 0x00,  // omega
+			0x0E, 0x10, 0x17, 0x12, 0x12, 0x12, 0x10, 0x0E,  // CT
+			0x04, 0x04, 0x1F, 0x04, 0x04, 0x00, 0x1F, 0x00  // +-
+			};
+	HD44780_Command(0x40);
+	for (int i = 0; i < 64; i++)
+		HD44780_Data(cc[i]);
 
-  lcd_put_cur(1, 3);
-  lcd_send_data(1);
+	HD44780_SetPos(1, 1); HD44780_Data(0);
+	HD44780_SetPos(1, 3); HD44780_Data(1);
+	HD44780_SetPos(1, 5); HD44780_Data(2);
+	HD44780_SetPos(1, 7); HD44780_Data(3);
+	HD44780_SetPos(1, 9); HD44780_Data(4);
+	HD44780_SetPos(1, 11); HD44780_Data(5);
+	HD44780_SetPos(1, 13); HD44780_Data(6);
+	HD44780_SetPos(1, 15); HD44780_Data(7);
 
-  lcd_put_cur(1, 5);
-  lcd_send_data(2);
+	/* USER CODE END 2 */
 
-  lcd_put_cur(1, 7);
-  lcd_send_data(3);
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1) {
+		/* USER CODE END WHILE */
 
-  lcd_put_cur(1, 9);
-  lcd_send_data(4);
-
-  lcd_put_cur(1, 11);
-  lcd_send_data(5);
-
-  lcd_put_cur(1, 13);
-  lcd_send_data(6);
-
-  lcd_put_cur(1, 15);
-  lcd_send_data(7);
-
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+		/* USER CODE BEGIN 3 */
+	}
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+	RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
+ * @brief I2C1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_I2C1_Init(void) {
 
-  /* USER CODE BEGIN I2C1_Init 0 */
+	/* USER CODE BEGIN I2C1_Init 0 */
 
-  /* USER CODE END I2C1_Init 0 */
+	/* USER CODE END I2C1_Init 0 */
 
-  /* USER CODE BEGIN I2C1_Init 1 */
+	/* USER CODE BEGIN I2C1_Init 1 */
 
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN I2C1_Init 2 */
+	/* USER CODE END I2C1_Init 1 */
+	hi2c1.Instance = I2C1;
+	hi2c1.Init.ClockSpeed = 100000;
+	hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+	hi2c1.Init.OwnAddress1 = 0;
+	hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+	hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+	hi2c1.Init.OwnAddress2 = 0;
+	hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+	hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+	if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN I2C1_Init 2 */
 
-  /* USER CODE END I2C1_Init 2 */
+	/* USER CODE END I2C1_Init 2 */
 
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+	/* USER CODE BEGIN MX_GPIO_Init_1 */
 
-  /* USER CODE END MX_GPIO_Init_1 */
+	/* USER CODE END MX_GPIO_Init_1 */
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
+	/* USER CODE BEGIN MX_GPIO_Init_2 */
 
-  /* USER CODE END MX_GPIO_Init_2 */
+	/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -245,18 +225,16 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
