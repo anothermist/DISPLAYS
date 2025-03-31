6f6f2bbf704a6bc6e5d8867d4bb66714e2f1ff53
anothermist <mistanother@gmail.com>
2025-04-01 04:07:50 +0500

    format

--- ST7920_HAL/Core/Src/delay.c
+++ ST7920_HAL/Core/Src/delay.c
@@ -5,25 +5,21 @@
  * rest is unchanged
  */
 
-
 #include "delay.h"
 #include "stm32f1xx.h"
 
 extern TIM_HandleTypeDef htim1;
 
-
-void delay_init ()
-{
+void delay_init() {
 	HAL_TIM_Base_Start(&htim1);  // change this according to ur setup
 }
 
-void delay_us (uint16_t delay)
-{
+void delay_us(uint16_t delay) {
 	__HAL_TIM_SET_COUNTER(&htim1, 0);  // reset the counter
-	while ((__HAL_TIM_GET_COUNTER(&htim1))<delay);  // wait for the delay to complete
+	while ((__HAL_TIM_GET_COUNTER(&htim1)) < delay)
+		;  // wait for the delay to complete
 }
 
-void delay_ms(uint16_t delay)
-{
-	HAL_Delay (delay);  // better use HAL_DElay in millis
+void delay_ms(uint16_t delay) {
+	HAL_Delay(delay);  // better use HAL_DElay in millis
 }
