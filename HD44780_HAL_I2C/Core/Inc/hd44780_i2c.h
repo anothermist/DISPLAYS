0fea89aadd01f0a08cd3afff47e1fc8fd72c40d9
anothermist <mistanother@gmail.com>
2025-04-06 06:16:31 +0500

    HD44780 with I2C

--- HD44780_HAL_I2C/Core/Inc/hd44780_i2c.h
+++ HD44780_HAL_I2C/Core/Inc/hd44780_i2c.h
@@ -2,16 +2,53 @@
 #define HD44780_I2C_H_
 
 #include "stm32f1xx_hal.h"
+#include "stdbool.h"
+#include "main.h"
 
 #define HD44780_INTERFACE hi2c1
 #define HD44780_ADDRESS 0x4E
 
-void HD44780_Init(void);   // initialize lcd
-void HD44780_Command(char cmd);  // send command to the lcd
-void HD44780_Data(char data);  // send data to the lcd
-void HD44780_String(char *str);  // send string to the lcd
-void HD44780_Position(int row, int col); // put cursor at the entered position row (0 or 1), col (0-15);
-void HD44780_Clear(void);
+/* List of COMMANDS */
+#define LCD_CLEARDISPLAY      0x01
+#define LCD_RETURNHOME        0x02
+#define LCD_ENTRYMODESET      0x04
+#define LCD_DISPLAYCONTROL    0x08
+#define LCD_CURSORSHIFT       0x10
+#define LCD_FUNCTIONSET       0x20
+#define LCD_SETCGRAMADDR      0x40
+#define LCD_SETDDRAMADDR      0x80
+
+//1) Entry mode Bitfields
+#define LCD_ENTRY_SH          0x01
+#define LCD_ENTRY_ID          0x02
+//2) Entry mode Bitfields
+#define LCD_ENTRY_SH          0x01
+#define LCD_ENTRY_ID          0x02
+//3) Display control
+#define LCD_DISPLAY_B         0x01
+#define LCD_DISPLAY_C         0x02
+#define LCD_DISPLAY_D         0x04
+//4) Shift control
+#define LCD_SHIFT_RL          0x04
+#define LCD_SHIFT_SC          0x08
+//5) Function set control
+#define LCD_FUNCTION_F        0x04
+#define LCD_FUNCTION_N        0x08
+#define LCD_FUNCTION_DL       0x10
+
+void HD44780_init_i2c(void);
+void HD44780_setCursor(uint8_t row, uint8_t col);
+void HD44780_1stLine(void);
+void HD44780_2ndLine(void);
+void HD44780_twoLines(void);
+void HD44780_oneLine(void);
+void HD44780_cursorShow(bool state);
+void HD44780_clear(void);
+void HD44780_display(bool state);
+void HD44780_shiftRight(uint8_t offset);
+void HD44780_shiftLeft(uint8_t offset);
+void HD44780_printf(const char *str, ...);
+void HD44780_PutSpecialSymbols(void);
 void HD44780_drawBigDigits(unsigned char digit, unsigned char place);
 
 #endif /* HD44780_I2C_H_ */
--- HD44780_HAL_I2C/Core/Src/hd44780_i2c.c
+++ HD44780_HAL_I2C/Core/Src/hd44780_i2c.c
@@ -1,8 +1,11 @@
 #include "hd44780_i2c.h"
+#include "string.h"
+#include "stdio.h"
+#include "stdarg.h"
 
 extern I2C_HandleTypeDef HD44780_INTERFACE;
 
-void HD44780_Command(char cmd) {
+void HD44780_writeCommand(char cmd) {
 	char data_u, data_l;
 	uint8_t data_t[4];
 	data_u = (cmd & 0xf0);
@@ -15,7 +18,7 @@ void HD44780_Command(char cmd) {
 			(uint8_t*) data_t, 4, 100);
 }
 
-void HD44780_Data(char data) {
+void HD44780_writeData(char data) {
 	char data_u, data_l;
 	uint8_t data_t[4];
 	data_u = (data & 0xf0);
@@ -28,13 +31,40 @@ void HD44780_Data(char data) {
 			(uint8_t*) data_t, 4, 100);
 }
 
-void HD44780_Clear(void) {
-	HD44780_Command(0x80);
-	for (int i = 0; i < 80; i++)
-		HD44780_Data(' ');
+#define LCD_MS_DELAY(X) (HAL_Delay(X))
+
+#define T_CONST   20
+static uint8_t DisplayControl = 0x0F;
+static uint8_t FunctionSet = 0x38;
+
+void HD44780_init_i2c(void){
+	// 4 bit initialisation
+	HAL_Delay(50);  // wait for >40ms
+	HD44780_writeCommand(0x30);
+	HAL_Delay(5);  // wait for >4.1ms
+	HD44780_writeCommand(0x30);
+	HAL_Delay(1);  // wait for >100us
+	HD44780_writeCommand(0x30);
+	HAL_Delay(10);
+	HD44780_writeCommand(0x20);  // 4bit mode
+	HAL_Delay(10);
+
+	// dislay initialisation
+	HD44780_writeCommand(0x28); // Function set --> DL=0 (4 bit mode), N = 1 (2 line display) F = 0 (5x8 characters)
+	HAL_Delay(1);
+	HD44780_writeCommand(0x08); //Display on/off control --> D=0,C=0, B=0  ---> display off
+	HAL_Delay(1);
+	HD44780_writeCommand(0x01);  // clear display
+	HAL_Delay(1);
+	HAL_Delay(1);
+	HD44780_writeCommand(0x06); //Entry mode set --> I/D = 1 (increment cursor) & S = 0 (no shift)
+	HAL_Delay(1);
+	HD44780_writeCommand(0x0C); //Display on/off control --> D = 1, C and B = 0. (Cursor and blink, last two bits)
+
+	HD44780_PutSpecialSymbols();
 }
 
-void HD44780_SetPos(int row, int col) {
+void HD44780_setCursor(uint8_t row, uint8_t col) {
 	switch (row) {
 	case 0:
 		col |= 0x80;
@@ -49,13 +79,73 @@ void HD44780_SetPos(int row, int col) {
 		col |= 0xC0 + 0x14;
 		break;
 	}
-
-	HD44780_Command(col);
+	HD44780_writeCommand(col);
 }
 
-void HD44780_String(char *str) {
-	while (*str)
-		HD44780_Data(*str++);
+void HD44780_1stLine(void) {
+	HD44780_setCursor(0, 0);
+}
+
+void HD44780_2ndLine(void) {
+	HD44780_setCursor(1, 0);
+}
+
+void HD44780_twoLines(void) {
+	FunctionSet |= (0x08);
+	HD44780_writeCommand(FunctionSet);
+}
+
+void HD44780_oneLine(void) {
+	FunctionSet &= ~(0x08);
+	HD44780_writeCommand(FunctionSet);
+}
+
+void HD44780_cursorShow(bool state) {
+	if (state) {
+		DisplayControl |= (0x03);
+		HD44780_writeCommand(DisplayControl);
+	} else {
+		DisplayControl &= ~(0x03);
+		HD44780_writeCommand(DisplayControl);
+	}
+}
+
+void HD44780_clear(void) {
+	HD44780_writeCommand(LCD_CLEARDISPLAY);
+	LCD_MS_DELAY(3);
+}
+
+void HD44780_display(bool state) {
+	if (state) {
+		DisplayControl |= (0x04);
+		HD44780_writeCommand(DisplayControl);
+	} else {
+		DisplayControl &= ~(0x04);
+		HD44780_writeCommand(DisplayControl);
+	}
+}
+
+void HD44780_shiftRight(uint8_t offset) {
+	for (uint8_t i = 0; i < offset; i++) {
+		HD44780_writeCommand(0x1c);
+	}
+}
+
+void HD44780_shiftLeft(uint8_t offset) {
+	for (uint8_t i = 0; i < offset; i++) {
+		HD44780_writeCommand(0x18);
+	}
+}
+
+void HD44780_printf(const char *str, ...) {
+	char stringArray[20];
+	va_list args;
+	va_start(args, str);
+	vsprintf(stringArray, str, args);
+	va_end(args);
+	for (uint8_t i = 0; i < strlen(stringArray) && i < 20; i++) {
+		HD44780_writeData((uint8_t) stringArray[i]);
+	}
 }
 
 char cc[] = { 0x07, 0x0F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F,
@@ -65,10 +155,10 @@ char cc[] = { 0x07, 0x0F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F,
 		0x1C, 0x1F, 0x1F, 0x1F, 0x00, 0x00, 0x00, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F,
 		0x1F, 0x1F, 0x1F, 0x1F, 0x1F };
 
-void HD44780_PutSpecialSymbols() {
-	HD44780_Command(0x40);
+void HD44780_PutSpecialSymbols(void) {
+	HD44780_writeCommand(0x40);
 	for (int i = 0; i < 64; i++)
-		HD44780_Data(cc[i]);
+		HD44780_writeData(cc[i]);
 }
 
 void HD44780_drawBigDigits(unsigned char digit, unsigned char place) {
@@ -76,182 +166,153 @@ void HD44780_drawBigDigits(unsigned char digit, unsigned char place) {
 	switch (digit) {
 
 	case 0:
-		HD44780_SetPos(0, place);
-		HD44780_Data(0);
-		HD44780_SetPos(0, place + 1);
-		HD44780_Data(1);
-		HD44780_SetPos(0, place + 2);
-		HD44780_Data(2);
-		HD44780_SetPos(1, place);
-		HD44780_Data(3);
-		HD44780_SetPos(1, place + 1);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 2);
-		HD44780_Data(5);
+		HD44780_setCursor(0, place);
+		HD44780_writeData(0);
+		HD44780_setCursor(0, place + 1);
+		HD44780_writeData(1);
+		HD44780_setCursor(0, place + 2);
+		HD44780_writeData(2);
+		HD44780_setCursor(1, place);
+		HD44780_writeData(3);
+		HD44780_setCursor(1, place + 1);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 2);
+		HD44780_writeData(5);
 		break;
 
 	case 1:
-		HD44780_SetPos(0, place);
-		HD44780_Data(1);
-		HD44780_SetPos(0, place + 1);
-		HD44780_Data(2);
-		HD44780_SetPos(0, place + 2);
-		HD44780_String(' ');
-		HD44780_SetPos(1, place);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 1);
-		HD44780_Data(7);
-		HD44780_SetPos(1, place + 2);
-		HD44780_Data(4);
+		HD44780_setCursor(0, place);
+		HD44780_writeData(1);
+		HD44780_setCursor(0, place + 1);
+		HD44780_writeData(2);
+		HD44780_setCursor(0, place + 2);
+		HD44780_printf(" ");
+		HD44780_setCursor(1, place);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 1);
+		HD44780_writeData(7);
+		HD44780_setCursor(1, place + 2);
+		HD44780_writeData(4);
 		break;
 
 	case 2:
-		HD44780_SetPos(0, place);
-		HD44780_Data(6);
-		HD44780_SetPos(0, place + 1);
-		HD44780_Data(6);
-		HD44780_SetPos(0, place + 2);
-		HD44780_Data(2);
-		HD44780_SetPos(1, place);
-		HD44780_Data(3);
-		HD44780_SetPos(1, place + 1);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 2);
-		HD44780_Data(4);
+		HD44780_setCursor(0, place);
+		HD44780_writeData(6);
+		HD44780_setCursor(0, place + 1);
+		HD44780_writeData(6);
+		HD44780_setCursor(0, place + 2);
+		HD44780_writeData(2);
+		HD44780_setCursor(1, place);
+		HD44780_writeData(3);
+		HD44780_setCursor(1, place + 1);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 2);
+		HD44780_writeData(4);
 		break;
 
 	case 3:
-		HD44780_SetPos(0, place);
-		HD44780_Data(6);
-		HD44780_SetPos(0, place + 1);
-		HD44780_Data(6);
-		HD44780_SetPos(0, place + 2);
-		HD44780_Data(2);
-		HD44780_SetPos(1, place);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 1);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 2);
-		HD44780_Data(5);
+		HD44780_setCursor(0, place);
+		HD44780_writeData(6);
+		HD44780_setCursor(0, place + 1);
+		HD44780_writeData(6);
+		HD44780_setCursor(0, place + 2);
+		HD44780_writeData(2);
+		HD44780_setCursor(1, place);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 1);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 2);
+		HD44780_writeData(5);
 		break;
 
 	case 4:
-		HD44780_SetPos(0, place);
-		HD44780_Data(3);
-		HD44780_SetPos(0, place + 1);
-		HD44780_Data(4);
-		HD44780_SetPos(0, place + 2);
-		HD44780_Data(7);
-		HD44780_SetPos(1, place);
-		HD44780_String(' ');
-		HD44780_SetPos(1, place + 1);
-		HD44780_String(' ');
-		HD44780_SetPos(1, place + 2);
-		HD44780_Data(7);
+		HD44780_setCursor(0, place);
+		HD44780_writeData(3);
+		HD44780_setCursor(0, place + 1);
+		HD44780_writeData(4);
+		HD44780_setCursor(0, place + 2);
+		HD44780_writeData(7);
+		HD44780_setCursor(1, place);
+		HD44780_printf(" ");
+		HD44780_setCursor(1, place + 1);
+		HD44780_printf(" ");
+		HD44780_setCursor(1, place + 2);
+		HD44780_writeData(7);
 		break;
 
 	case 5:
-		HD44780_SetPos(0, place);
-		HD44780_Data(3);
-		HD44780_SetPos(0, place + 1);
-		HD44780_Data(6);
-		HD44780_SetPos(0, place + 2);
-		HD44780_Data(6);
-		HD44780_SetPos(1, place);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 1);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 2);
-		HD44780_Data(5);
+		HD44780_setCursor(0, place);
+		HD44780_writeData(3);
+		HD44780_setCursor(0, place + 1);
+		HD44780_writeData(6);
+		HD44780_setCursor(0, place + 2);
+		HD44780_writeData(6);
+		HD44780_setCursor(1, place);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 1);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 2);
+		HD44780_writeData(5);
 		break;
 
 	case 6:
-		HD44780_SetPos(0, place);
-		HD44780_Data(0);
-		HD44780_SetPos(0, place + 1);
-		HD44780_Data(6);
-		HD44780_SetPos(0, place + 2);
-		HD44780_Data(6);
-		HD44780_SetPos(1, place);
-		HD44780_Data(3);
-		HD44780_SetPos(1, place + 1);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 2);
-		HD44780_Data(5);
+		HD44780_setCursor(0, place);
+		HD44780_writeData(0);
+		HD44780_setCursor(0, place + 1);
+		HD44780_writeData(6);
+		HD44780_setCursor(0, place + 2);
+		HD44780_writeData(6);
+		HD44780_setCursor(1, place);
+		HD44780_writeData(3);
+		HD44780_setCursor(1, place + 1);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 2);
+		HD44780_writeData(5);
 		break;
 
 	case 7:
-		HD44780_SetPos(0, place);
-		HD44780_Data(1);
-		HD44780_SetPos(0, place + 1);
-		HD44780_Data(1);
-		HD44780_SetPos(0, place + 2);
-		HD44780_Data(2);
-		HD44780_SetPos(1, place);
-		HD44780_String(' ');
-		HD44780_SetPos(1, place + 1);
-		HD44780_String(' ');
-		HD44780_SetPos(1, place + 2);
-		HD44780_Data(7);
+		HD44780_setCursor(0, place);
+		HD44780_writeData(1);
+		HD44780_setCursor(0, place + 1);
+		HD44780_writeData(1);
+		HD44780_setCursor(0, place + 2);
+		HD44780_writeData(2);
+		HD44780_setCursor(1, place);
+		HD44780_printf(" ");
+		HD44780_setCursor(1, place + 1);
+		HD44780_printf(" ");
+		HD44780_setCursor(1, place + 2);
+		HD44780_writeData(7);
 		break;
 
 	case 8:
-		HD44780_SetPos(0, place);
-		HD44780_Data(0);
-		HD44780_SetPos(0, place + 1);
-		HD44780_Data(6);
-		HD44780_SetPos(0, place + 2);
-		HD44780_Data(2);
-		HD44780_SetPos(1, place);
-		HD44780_Data(3);
-		HD44780_SetPos(1, place + 1);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 2);
-		HD44780_Data(5);
+		HD44780_setCursor(0, place);
+		HD44780_writeData(0);
+		HD44780_setCursor(0, place + 1);
+		HD44780_writeData(6);
+		HD44780_setCursor(0, place + 2);
+		HD44780_writeData(2);
+		HD44780_setCursor(1, place);
+		HD44780_writeData(3);
+		HD44780_setCursor(1, place + 1);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 2);
+		HD44780_writeData(5);
 		break;
 
 	case 9:
-		HD44780_SetPos(0, place);
-		HD44780_Data(0);
-		HD44780_SetPos(0, place + 1);
-		HD44780_Data(6);
-		HD44780_SetPos(0, place + 2);
-		HD44780_Data(2);
-		HD44780_SetPos(1, place);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 1);
-		HD44780_Data(4);
-		HD44780_SetPos(1, place + 2);
-		HD44780_Data(5);
+		HD44780_setCursor(0, place);
+		HD44780_writeData(0);
+		HD44780_setCursor(0, place + 1);
+		HD44780_writeData(6);
+		HD44780_setCursor(0, place + 2);
+		HD44780_writeData(2);
+		HD44780_setCursor(1, place);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 1);
+		HD44780_writeData(4);
+		HD44780_setCursor(1, place + 2);
+		HD44780_writeData(5);
 		break;
 	}
-}
-
-void HD44780_Init(void) {
-	// 4 bit initialisation
-	HAL_Delay(50);  // wait for >40ms
-	HD44780_Command(0x30);
-	HAL_Delay(5);  // wait for >4.1ms
-	HD44780_Command(0x30);
-	HAL_Delay(1);  // wait for >100us
-	HD44780_Command(0x30);
-	HAL_Delay(10);
-	HD44780_Command(0x20);  // 4bit mode
-	HAL_Delay(10);
-
-	// dislay initialisation
-	HD44780_Command(0x28); // Function set --> DL=0 (4 bit mode), N = 1 (2 line display) F = 0 (5x8 characters)
-	HAL_Delay(1);
-	HD44780_Command(0x08); //Display on/off control --> D=0,C=0, B=0  ---> display off
-	HAL_Delay(1);
-	HD44780_Command(0x01);  // clear display
-	HAL_Delay(1);
-	HAL_Delay(1);
-	HD44780_Command(0x06); //Entry mode set --> I/D = 1 (increment cursor) & S = 0 (no shift)
-	HAL_Delay(1);
-	HD44780_Command(0x0C); //Display on/off control --> D = 1, C and B = 0. (Cursor and blink, last two bits)
-
-	HD44780_PutSpecialSymbols();
-	HD44780_Clear();
-
 }
